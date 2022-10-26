4f7d3c55693b3fe2bb3913f85ffbe2a3a7899ff5
sniperdev <62942349+sniperdev@users.noreply.github.com>
2022-10-26 19:37:08 +0200

    <build> add react

--- server/app.js
+++ server/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "todolist",
+});
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
@@ -9,3 +16,12 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("listening on port " + port);
 });
+
+connection.connect();
+connection.query("SELECT * FROM todolist", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+// connection.query('insert into todolist (task) values ("test3");');
+// connection.query("delete from todolist");
+connection.end();
